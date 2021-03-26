1fb6a0b6ee12420a26ae829252098dade9f625ca
Shivateja Burle <shivateja.idizital@gmail.com>
2021-03-26 21:34:18 +0530

    You should not change master. Be careful.

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require("http");
+const _ = require("lodash");
+
 http
-  .createServer((req, res) => {   
-      console.log(res);
+  .createServer((req, res) => {
+    switch (request.url) {
+      case "/":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
         response.write("<h1>Home Page!</h1>");
         response.end();
+        break;
+      
+        case "/about":
+          response.writeHead(200, {
+            "Content-Type": "text/html",
+          });
+          response.write("<h2>About Page!</h2>");
+          response.end();
+          break;
+    }
   })
   .listen(8080);
