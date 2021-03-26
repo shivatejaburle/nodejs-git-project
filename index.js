d5e1dc67e775171367073c962e212cd917fee0e7
Shivateja Burle <shivateja.idizital@gmail.com>
2021-03-26 21:40:35 +0530

    Check the branch always you are working

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const http = require("http");
-const _ = require("lodash");
-
 http
-  .createServer((req, res) => {
-    switch (request.url) {
-      case "/":
-        response.writeHead(200, {
-          "Content-Type": "text/html",
-        });
+  .createServer((req, res) => {   
+      console.log(res);
         response.write("<h1>Home Page!</h1>");
         response.end();
-        break;
-      
-        case "/about":
-          response.writeHead(200, {
-            "Content-Type": "text/html",
-          });
-          response.write("<h2>About Page!</h2>");
-          response.end();
-          break;
-    }
   })
   .listen(8080);
