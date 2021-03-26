7afd30a49fddb1eaf862f416f2d2e535a829800d
Shivateja Burle <shivateja.idizital@gmail.com>
2021-03-26 20:11:07 +0530

    Added 404 Feature

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const http = require("http");
+const _ = require("lodash");
+
 http
-  .createServer((req, res) => {   
-      console.log(res);
+  .createServer((req, res) => {
+    switch (request.url) {
+      case "/":
+        response.writeHead(200, {
+          "Content-Type": "text/html",
+        });
         response.write("<h1>Home Page!</h1>");
         response.end();
+        break;
+      
+      default:
+        response.writeHead(404, {
+          "Content-Type": "text/html",
+        });
+        console.log("404 page has been visited!");
+        response.write("<p>404 Page Not found!</p>");
+        response.end();
+        break;
+    }
   })
   .listen(8080);
